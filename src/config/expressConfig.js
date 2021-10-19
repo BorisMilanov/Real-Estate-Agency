13e7f546a58ac1abfc5ccc658162eb00549e460f
Boris <boris.mil.96@gmail.com>
2021-10-19 22:19:34 +0300

    config serve files

---
+++ src/config/expressConfig.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const path = require('path');
+function staticFilesServe(app) {
+    app.use('/static',express.static(path.join(__dirname,'../public')));
+};
+
+module.exports = staticFilesServe;
--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@ const express = require('express');
 const app = express();
 const port = 5000;
 
-const hbsConfig = require('./config/handlebarsConfig')
-hbsConfig(app)
+const hbsConfig = require('./config/handlebarsConfig');
+hbsConfig(app);
+
+const serveFiles = require('./config/expressConfig');
+serveFiles(app);
 
 app.get('/',(req,res)=>{
     res.render('home',{layout:false});
